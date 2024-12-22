67ba0f37d2540b10055c9d2583ab5f74d7b9c61e
Tarikuzzaman12 <tarikuzzamantuhin920@gmail.com>
2024-12-22 12:24:53 +0600

    added Login Form

--- src/main.jsx
+++ src/main.jsx
@@ -1,59 +1,64 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-import MainLayout from './Layout/MainLayout';
-import AllRooms from './pages/AllRooms';
-import MyBookingRoom from './pages/MyBookingRoom';
-import Home from './pages/Home';
-import AuthProvider from './provider/AuthProvider';
-import Register from './pages/Register';
-import { ToastContainer } from 'react-toastify';
+import MainLayout from "./Layout/MainLayout";
+import AllRooms from "./pages/AllRooms";
+import MyBookingRoom from "./pages/MyBookingRoom";
+import Home from "./pages/Home";
+import AuthProvider from "./provider/AuthProvider";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import { ToastContainer } from "react-toastify";
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
-    children:[
+    children: [
       {
-        path:"/",
-        element:<Home></Home>
+        path: "/",
+        element: <Home></Home>,
       },
       {
-        path:"all-rooms",
-        element:<AllRooms></AllRooms>
+        path: "all-rooms",
+        element: <AllRooms></AllRooms>,
       },
       {
-        path:"my-booking-room",
-        element:<MyBookingRoom></MyBookingRoom>
+        path: "my-booking-room",
+        element: <MyBookingRoom></MyBookingRoom>,
       },
       {
-        path:"register",
-        element:<Register></Register>
+        path: "login",
+        element: <Login></Login>,
       },
-    ]
+      {
+        path: "register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AuthProvider>
-    <ToastContainer
-      position="top-center"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </AuthProvider>
-    </StrictMode>,
-)
+  </StrictMode>
+);
