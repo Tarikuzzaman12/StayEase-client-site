e453b559bd20804831d8828a889186eb0b661d1f
Tarikuzzaman12 <tarikuzzamantuhin920@gmail.com>
2024-12-25 15:45:05 +0600

    added map in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Slider from '../components/Slider';
 import FeaturesRooms from '../components/FeaturesRooms';
+import Map from '../components/Map';
 
 const Home = () => {
     return (
         <div>
             <Slider></Slider>
             <FeaturesRooms></FeaturesRooms>
+          <div className="my-[100px] w-11/12 mx-auto">  <Map></Map></div>
         </div>
     );
 };
